3849b672254586cc0534b8dbae478eb0a6acc711
ApiokK <quadrifoil@gmail.com>
2019-10-13 12:55:39 +0300

    tasks c1,c2 lesson 4 complete

--- src/by/it/apiok/lesson04/TaskC1.java
+++ src/by/it/apiok/lesson04/TaskC1.java
@@ -1,22 +1,20 @@
 package by.it.apiok.lesson04;
 
+import java.util.Scanner;
+
 /*
-Напишите программу которая спрашивает у пользователя:
-Какую вы хотите зарплату в $$$?
+        Напишите программу которая спрашивает у пользователя:
+        Какую вы хотите зарплату в $$$?
+        После ввода суммы, программа анализирует полученное значение
+        и если введенная цифра меньше 300 или больше 3000 программа выводит на экран:
+        Мы вам перезвоним!
 
-После ввода суммы, программа анализирует полученное значение
-и если введенная цифра меньше 300 или больше 3000 программа выводит на экран:
-Мы вам перезвоним!
-
-Иначе выводит в цикле по переменной int month от 0 до 14 рассчитанную зарплату.
+        Иначе выводит в цикле по переменной int month от 0 до 14 рассчитанную зарплату.
 В летние месяцы - выводится введенная сумма с названием месяца.
-В несуществующие месяцы 0, 13 и 14 выводится сумма в $0.0
+        В несуществующие месяцы 0, 13 и 14 выводится сумма в $0.0
 в остальные месяцы выводится указанная сумма с премией в 50%.
-
 Сделайте так, чтобы в тот месяц, когда получилась зарплата в $666 цикл после вывода прерывался.
-
 Пример работы программы (у вас должно быть так же, до символа):
-
 Какую вы хотите зарплату в $$$?
 2000
 За месяц 0 начислено $0.0
@@ -37,6 +35,76 @@ package by.it.apiok.lesson04;
 
 */
 public class TaskC1 {
+    public static void main(String[] args) {
+        System.out.println("Какую вы хотите зарплату в $$$?");
+        Scanner sc = new Scanner(System.in);
+        double value = sc.nextInt();
 
-
+        if (value < 300 || value > 3000) {
+            System.out.println("Мы вам перезвоним!");
+        } else {
+            for (int i = 0; i <= 14; i++) {
+                if (i > 12 || i < 1) {
+                    System.out.println("За месяц " + i + " начислено $0.0");
+                } else {
+                    double onCard=0.0;
+                    switch (i) {
+                        case 1:
+                            onCard = value * 1.5;
+                            System.out.println("За январь начислено $" + onCard);
+                            break;
+                        case 2:
+                            onCard = value * 1.5;
+                            System.out.println("За февраль начислено $" + onCard);
+                            break;
+                        case 3:
+                            onCard = value * 1.5;
+                            System.out.println("За март начислено $" + onCard);
+                            break;
+                        case 4:
+                            onCard = value * 1.5;
+                            System.out.println("За апрель начислено $" + onCard);
+                            break;
+                        case 5:
+                            onCard = value * 1.5;
+                            System.out.println("За май начислено $" + onCard);
+                            break;
+                        case 6:
+                            onCard = value;
+                            System.out.println("За июнь начислено $" + onCard);
+                            break;
+                        case 7:
+                            onCard = value;
+                            System.out.println("За июль начислено $" + onCard);
+                            break;
+                        case 8:
+                            onCard = value;
+                            System.out.println("За август начислено $" + onCard);
+                            break;
+                        case 9:
+                            onCard = value * 1.5;
+                            System.out.println("За сентябрь начислено $" + onCard);
+                            break;
+                        case 10:
+                            onCard = value * 1.5;
+                            System.out.println("За октябрь начислено $" + onCard);
+                            break;
+                        case 11:
+                            onCard = value * 1.5;
+                            System.out.println("За ноябрь начислено $" + onCard);
+                            break;
+                        case 12:
+                            onCard = value * 1.5;
+                            System.out.println("За декабрь начислено $" + onCard);
+                            break;
+                    }
+                    if (onCard == 666.0) break;
+                }
+            }
+        }
+    }
 }
+
+
+
+
--- src/by/it/apiok/lesson04/TaskC2.java
+++ src/by/it/apiok/lesson04/TaskC2.java
@@ -1,31 +1,54 @@
 package by.it.apiok.lesson04;
 
-/*
+import java.util.Scanner;
 
-На входе вводится число и месяц 2018 года.
+/*
+            На входе вводится число и месяц 2018 года.
 Программа должна выводить номер дня года, считая 1 января первым днем,
 а 31 декабря 365-м.
 
 Пример:
 Ввод:
 1 1
-
 Вывод
 1
 
 Ввод:
 31 12
-
 Вывод
 365
 
 Ввод:
 13 9
-
 Вывод
 256
 
  */
 public class TaskC2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        int month = sc.nextInt();
+        int d=0;
+        int dd=0;
 
+        for (int i = 0; i <month; i++) {
+            switch (i) {
+                case 1: d=31; break;
+                case 2: d=28; break;
+                case 3: d=31; break;
+                case 4: d=30; break;
+                case 5: d=31; break;
+                case 6: d=30; break;
+                case 7: d=31; break;
+                case 8: d=31; break;
+                case 9: d=30; break;
+                case 10: d=31; break;
+                case 11: d=30; break;
+                case 12: d=31; break;
+            }
+            dd=d+dd;
+        }
+        System.out.println(day+dd);
+    }
 }
