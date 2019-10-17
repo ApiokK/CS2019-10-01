e8375c4551a3536c663cceb59439537a1725d786
ApiokK <quadrifoil@gmail.com>
2019-10-17 07:25:32 +0300

    lesson5 tasks a+b last

--- src/by/it/apiok/lesson05/TaskB3.java
+++ src/by/it/apiok/lesson05/TaskB3.java
@@ -1,5 +1,4 @@
 package by.it.apiok.lesson05;
-
 /*
 Минимум и максимум
             1. Создать массив m на 10 целых чисел.
@@ -9,31 +8,28 @@ package by.it.apiok.lesson05;
 исходного массива и выведите их в виде:
 Index of first element=???
 Index of last element=???
-
 Например, для такого ввода
 123 99 88 77 66 5 4 3 0 2
-
 ожидается такой вывод:
 Index of first element=9
 Index of last element=1
-
 */
-
+import java.util.Arrays;
 import java.util.Scanner;
-
 public class TaskB3 {
     public static void main(String[] args) {
         int[] m = new int[10];
         int a, b, t;
         int first = 0, last = 0;
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < m.length; i++) {
+        for (int i = 0; i <= 9; i++) {
             m[i]=sc.nextInt();
         }
-        m[0] = first;
-        m[9] = last;
-        for(a=1; a < m.length; a++) {
-            for(b=m.length; b >= a; b--) {
+        System.out.println(Arrays.toString(m));
+        first = m[0];
+        last = m[9];
+        for(a=1; a <= 9; a++) {
+            for(b=9; b >= a; b--) {
                 if(m[b-1] > m[b]) { // if out of order
                     // exchange elements
                     t = m[b-1];
@@ -42,6 +38,7 @@ public class TaskB3 {
                 }
             }
         }
+        System.out.println(Arrays.toString(m));
         for (int i = 0; i < m.length; i++) {
            if (m[i]==first) System.out.println("Index of first element="+i);
            if (m[i]==last) System.out.println("Index of last element="+i);
